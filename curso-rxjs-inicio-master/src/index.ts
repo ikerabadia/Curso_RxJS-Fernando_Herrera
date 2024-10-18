82f9e793e175e2c7fd8e0fbb836e50d7c5d7ec54
iker.abadia <iker.abadia@bosonit.com>
2024-10-18 10:08:06 +0200

    17. Subscription y unsubscribe [FINISHED]

--- curso-rxjs-inicio-master/src/index.ts
+++ curso-rxjs-inicio-master/src/index.ts
@@ -1,1 +1,33 @@
-console.log("Iker Abadia");
+import { Observable, Observer } from "rxjs";
+
+const observer: Observer<any> = {
+  next: (value) => console.log("next: ", value),
+  error: (error) => console.warn("error: ", error),
+  complete: () => console.info("completado")
+};
+
+const intervalo$ = new Observable<number>((subscriber) => {
+  let segundo = 0;
+  const interval = setInterval(() => {
+    subscriber.next(segundo);
+    console.log(segundo);
+    segundo++;
+  }, 1000);
+
+  return () => {
+    clearInterval(interval);
+    console.log("Intervalo destruido");
+  };
+});
+
+const subs1 = intervalo$.subscribe();
+const subs2 = intervalo$.subscribe();
+const subs3 = intervalo$.subscribe();
+
+setTimeout(() => {
+  subs1.unsubscribe();
+  subs2.unsubscribe();
+  subs3.unsubscribe();
+
+  console.log("Completado timeout");
+}, 3000);
